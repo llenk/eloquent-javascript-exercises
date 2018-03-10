eedfeaa13a6a854711e7ad7f04643368ac42868a
llenk <ellenkeal@gmail.com>
2018-03-10 10:35:30 -0600

    chapter 3 exercises

--- chapter3.js
+++ chapter3.js
@@ -6,3 +6,32 @@ var min = function(a, b) {
     return a;
   }
 };
+
+var isEven = function(n) {
+  if (n == 1) {
+    return false;
+  }
+  if else (n == 0) {
+    return true;
+  }
+  if else (n < 0) {
+    return isEven(n+2);
+  }
+  else {
+    return isEven(n-2);
+  }
+};
+
+var countChar = function(string, char) {
+  let num = 0;
+  for (let i=0; i<string.length; i++) {
+    if (string[i] === char) {
+      num++;
+    }
+  }
+  return num;
+};
+
+var countBs = function(string) {
+  return countChar(string, 'B');
+}
